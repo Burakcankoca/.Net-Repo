0d7af05d4cafdd779c4c881c4d11a05dd8989340
Burakcankoca <burakcankoca@gmail.com>
2025-08-14 07:03:57 +0300

    Rename and restructure Car Rent Automation System
    Renamed project files and directories from 'Assignment -3 (Grup calismasi - 08.08)' to 'Car Rent Automation System' for improved clarity and organization. Removed all build output and intermediate files from the old structure.

---
+++ Car Rent Automation System/Enums/VehicleTypes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment__3__Grup_calismasi___08._08_.Enums
+{
+    public enum VehicleTypes
+    {
+        Otomobile,SUV,Minibus,ELectricalCar
+    }
+}
---
+++ Car Rent Automation System/Exceptions/FullyNumbered.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment__3__Grup_calismasi___08._08_.Exceptions
+{
+    public class FullyNumbered : Exception
+    {
+        public FullyNumbered(string message):base(message)
+        {
+            
+        }
+    }
+}
